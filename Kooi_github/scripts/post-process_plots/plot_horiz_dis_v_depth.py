20dd111761e4478576a4ac89f42626859c7be111
Delphine Lobelle <delphine.lobelle@soton.ac.uk>
2020-07-31 16:07:46 +0200

    produces plot for horizontal distance vs depth for all sizes for 2004 and 920kgm-3

---
+++ Kooi_github/scripts/post-process_plots/plot_horiz_dis_v_depth.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 18:15:42 2020
+
+@author: Lobel001
+"""
+# 30/07/20- This has been written for 920 kg m-3 only. 
+
+from math import sin, cos, sqrt, atan2, radians
+from geopy import distance
+
+import matplotlib 
+import matplotlib.pyplot as plt
+from netCDF4 import Dataset
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import numpy.matlib 
+#import math
+#from pylab import *
+import cmocean
+import os #, fnmatch 
+#import cartopy
+import pickle 
+np.seterr(divide='ignore', invalid='ignore')
+import warnings
+warnings.filterwarnings("ignore", "Mean of empty slice")
+
+rho = '920' # [kgm-3]: density of the plastic 
+res = '2x2' # [deg]: resolution of the global release of particles
+size = 'r1e-07' # [m]: size of plastic
+loc = 'global'
+
+if size == 'r1e-02':
+    size_fn = 'r0.01'
+else:
+    size_fn = size #'r1e-02'
+
+dirread = '/Users/Lobel001/Desktop/Local_postpro/Kooi_data/data_output/rho_'+rho+'kgm-3/res_'+res+'/'+size+'/'
+dirwrite = '/Users/Lobel001/Desktop/Local_postpro/Kooi_data/post_pro_data/'
+dirwritefigs = '/Users/Lobel001/Desktop/Local_postpro/Kooi_figures/rho_'+rho+'kgm-3/res_'+res+'/'+size+'/'
+
+plt.close("all")
+
+#%%
+#list_aux = os.listdir(dirread) # complete list of all files in dirread
+
+yr_st = 2004
+numyrs = 1
+yrs = np.arange(yr_st,yr_st+numyrs)
+seas = ['DJF', 'MAM', 'JJA', 'SON']
+
+
+
+
+for idx,s in enumerate(seas): #, 'JJA', 'SON']): # the 4 seasons 
+    for idy,yr in enumerate(yrs): #yrs): #2011):
+        print(s, yr)
+        
+        if size == 'r1e-05' or size == 'r1e-07': # for those sizes that have 3 latitudinal bands 
+            loc = ['south_global','eq_global','north_global']
+            for idz, lo in enumerate(loc):
+                fname = '%s_%s_%s_3D_grid2x2_rho%s_%s_90days_60dtsecs_12hrsoutdt.nc' % (lo, s, yr, rho, size)
+                if not os.path.isfile(dirread+fname):
+                    print('%s not found' %fname)
+                else:          
+                    data = Dataset(dirread+fname,'r') 
+                    time = data.variables['time'][0,:]/86400
+                    if idz == 0:
+                        lons = data.variables['lon'][:]
+                        lats = data.variables['lat'][:]
+                        depths =data.variables['z'][:]
+                    else:
+                        lons = np.vstack((lons,data.variables['lon'][:])) 
+                        lats= np.vstack((lats,data.variables['lat'][:]))
+                        depths =np.vstack((depths,data.variables['z'][:]))
+        elif size == 'r1e-03' or size == 'r1e-06':
+            fname = 'global_%s_%s_3D_grid2x2_allrho_allr_90days_60dtsecs_12hrsoutdt.nc' % (s, yr)
+            dirread = '/Users/Lobel001/Desktop/Local_postpro/Kooi_data/data_output/allrho/res_'+res+'/allr/'
+            if not os.path.isfile(dirread+fname):
+                print('%s not found' %fname)
+            else:          
+                data = Dataset(dirread+fname,'r')  
+                time = data.variables['time'][0,:]/86400
+                rho2 = float(rho) #rho_all[ii]
+                size2 = float(size[1:len(size)]) #size_all[ii]
+                rho_output=data.variables['rho_pl'][:]
+                r_output=data.variables['r_pl'][:]
+               
+                inds = np.where((rho_output==rho2) & (r_output.data==size2))[0]
+               
+                lons=data.variables['lon'][inds] #[:]
+                lats=data.variables['lat'][inds] 
+                depths =data.variables['z'][inds]
+        else:        
+            fname = 'global_%s_%s_3D_grid2x2_rho%s_%s_90days_60dtsecs_12hrsoutdt.nc' % (s, yr, rho, size_fn)
+            if not os.path.isfile(dirread+fname):
+                print('%s not found' %fname)
+            else:          
+                data = Dataset(dirread+fname,'r') 
+                time = data.variables['time'][0,:]/86400
+                lons=data.variables['lon'][:] 
+                lats=data.variables['lat'][:] 
+                depths =data.variables['z'][:]
+        
+        time = time - time[0]
+        
+        dist = np.zeros((depths.shape[0],time.shape[0]-1)) #lons.shape[0]
+        
+        #z_max2 = [0, ] * depths.shape[0]
+        for i in range(depths.shape[0]): #9582: number of particles 
+            
+            z2 = [0,] * depths.shape[1]
+            for ii in range(2,depths.shape[1]):
+                z2[ii-1] = depths[i,ii]-depths[i,ii-1] #np.where(depths[i,ii]<depths[i,ii-1])[0]
+            zf = np.where(np.array(z2) < 0.)[:]
+            #print(zf)
+            zind = zf[0][0] if np.array(zf).any() else []
+            # z_max2[i] = depths[i,zind] if np.array(zind).any() else 1.0
+            #tind = np.where()
+        
+        # for i in range(lons.shape[0]):
+            if np.array(zind).any():
+                for t in range(np.array(zind)): #time.shape[0]-1): 
+                    sinks = np.where(depths[0,:]>1.0)
+                    if lats[i,t+1].any(): #and np.array(sinks).any():
+                        loc1 = (lats[i,t],lons[i,t])
+                        loc2 = (lats[i,t+1],lons[i,t+1])                        
+                        dist[i,t] = distance.geodesic(loc1,loc2).km 
+                    else:
+                        dist[i,t] = np.nan
+ #%%                   
+    distnan = np.where(dist==0,np.nan,dist)    
+            
+    distcum = np.cumsum(distnan,axis = 1)
+    n = np.array(([[np.nan, ]] * distcum.shape[0]))
+    dist_p = np.append(distcum,n, axis= 1)
+    
+    lats_p = np.tile(lats[:,0],(lats.shape[1],1)).T
+    
+    # median depth 
+    z_med = np.nanmedian(depths[depths>1].ravel().data)
+    dist_med = np.nanmedian(dist_p.ravel().data)
+    
+    # to get the cmap for line plot below, need to get it from scatterplot
+    fig1 = plt.figure(figsize=(15,10))
+    cmap = plt.cm.get_cmap('coolwarm',7)
+    scat = plt.scatter(dist_p,(depths*-1), vmin = -70, vmax = 70, c = lats_p, cmap = cmap) #, alpha = 0.4) #, cbarlabel = 'initial latitude') # 
+    
+    #%% 20/07/20- Line plot with marker depth vs horizontal distance 
+    
+    fig2 = plt.figure(figsize=(15,10))
+    cbar = plt.colorbar(scat,label = 'initial latitude')
+    #cmap = plt.cm.get_cmap('coolwarm',7)
+    plot_idx = np.random.permutation(dist_p.shape[0])
+    
+    for i in range(distcum.shape[0]):
+        ii = plot_idx[i]
+        
+        if lats[ii,0]<-50.:
+            rgb = cmap(0)[:3]
+        elif lats[ii,0]<-30. and lats[ii,0]>=-50.:
+            rgb = cmap(1)[:3]
+        elif lats[ii,0]<-10. and lats[ii,0]>=-30.:
+            rgb = cmap(2)[:3]
+        elif lats[ii,0]<10. and lats[ii,0]>=-10.:
+            rgb = cmap(3)[:3]
+            
+        elif lats[ii,0]<30. and lats[ii,0]>=10.:
+            rgb = cmap(4)[:3]
+        elif lats[ii,0]<50. and lats[ii,0]>=30.:
+            rgb = cmap(5)[:3]
+        elif lats[ii,0]>=50.:
+            rgb = cmap(6)[:3]
+        
+        #test = np.where()    
+        plt.plot(dist_p[ii,:],(depths[ii,:]*-1), c = rgb, linewidth=3, alpha = 0.7)# 0.6) #alpha = 0.6,
+        plt.plot(dist_p[ii,-1],(depths[ii,-1]*-1), marker = 'o', c = rgb, markersize=10, markeredgecolor='black',alpha = 0.7) 
+    
+    
+    plt.axvline(x=dist_med, color = 'k', linewidth = 4)
+    plt.axhline(y=z_med*-1, color = 'k', linewidth = 4)
+    plt.ylim(top=0, bottom =-160) 
+    plt.xlim(left=0, right = 2500)
+    ax = plt.gca()
+    #ax.set_facecolor('lightgrey')   
+    plt.ylabel('Depth [m]', size = 20)
+    plt.xlabel('Horizontal distance [km]', size = 20)
+    font = {'size'   : 20} ##'family' : 'normal',
+            #'weight' : 'bold',
+    plt.title(str(s)+' Horizontal distance travelled by rho ='+str(rho)+', size ='+str(size) ,size = 20)
+    
+    matplotlib.rc('font', **font)
+
+
+#%% 30/07/20- No longer want scatterplot
+# plot_idx = np.random.permutation(dist_p.shape[0])
+
+# fig = plt.figure(figsize=(15,10))
+# cmap = plt.cm.get_cmap('coolwarm',7) #cmocean.cm.delta,8)
+# # cm = plt.cm.get_cmap('viridis',len(years))
+
+# s = plt.scatter(dist_p[plot_idx,:],(depths[plot_idx,:]*-1), vmin = -70, vmax = 70, c = lats_p[plot_idx], cmap = cmap) #, alpha = 0.4) #, cbarlabel = 'initial latitude') # 
+# #plt.plot(dist_p[plot_idx,:],(depths[plot_idx,:]*-1),'k')
+# plt.axvline(x=dist_med, color = 'k', linewidth = 3)
+# plt.axhline(y=z_med*-1, color = 'k', linewidth = 3)
+# plt.ylim(top=0) 
+# plt.xlim(left=0)
+# plt.ylabel('Depth [m]', size = 18)
+# plt.xlabel('Horizontal distance [km]', size = 18)
+# plt.colorbar(label = 'initial latitude')    
+# #ax = plt.gca()
+# #ax.set_facecolor('grey')    
+
+  
+#%% 30/07/20- This worked only if the initial latitudes were equidistant, however there are more particles/ocean area in southern hemisphere
+# ind_sortlat = numpy.argsort(lats[:,0])
+# cmap_sort= cmocean.cm.delta(np.linspace(0, 1, 9620))
+
+# fig2 = plt.figure(figsize=(15,10))
+# plt.colorbar(s)
+# for ii in range(dist2.shape[0]):
+#     order = ind_sortlat[ii]
+#     plt.plot(dist_p[order,:],(depths[order,:]*-1), '-o', c = cmap_sort[ii])
+
+# plt.ylim(top=0) 
+# plt.xlim(left=0)
+
+#fig2.colorbar(cmap_sort)
+#%%
+
+# for             
+#     plt.plot(dist_p[500],(depths[500]*-1),'-o')            
+
+# for idx,s in enumerate(seas): #, 'JJA', 'SON']): # the 4 seasons 
+#     for idy,yr in enumerate(yrs): #yrs): #2011):
+#         print(s, yr)
+#         fname = 'global_%s_%s_3D_grid2x2_rho%s_%s_90days_60dtsecs_12hrsoutdt.nc' % (s, yr, rho, size_fn)
+#         if not os.path.isfile(dirread+fname):
+#             print('%s not found' %fname)
+#         else:          
+#             data = Dataset(dirread+fname,'r') 
+#             time = data.variables['time'][0,:]/86400
+#             lons=data.variables['lon'][:] 
+#             lats=data.variables['lat'][:] 
+#             depths =data.variables['z'][:]
+    
+#             time = time - time[0]
+            
+#             dist = np.zeros((depths.shape[0],time.shape[0]-1)) #lons.shape[0]
+            
+            
+#             for i in range(lons.shape[0]):
+#                 for t in range(time.shape[0]-1):                   
+#                     if lats[i,t+1].any():
+#                         loc1 = (lats[i,t],lons[i,t])
+#                         loc2 = (lats[i,t+1],lons[i,t+1])                        
+#                         dist[i,t] = distance.geodesic(loc1,loc2).km 
+#                     else:
+#                         dist[i,t] = np.nan
